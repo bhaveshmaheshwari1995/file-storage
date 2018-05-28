b5d6f98d77fa4b13a3093b53df68b66c289fc990
bhavesh maheshwari <bhavesh.maheshwari08@gmail.com>
2018-05-29 00:34:09 +0530

    Suggestion added  - comman fields in getDetails()  - open() uniform output i.e. Buffer  - rename functionality added  - minor changes

--- test/test-local.js
+++ test/test-local.js
@@ -1,8 +1,9 @@
-var fileStorage = require('./file-storage');
+var fileStorage = require('../../file-storage');
 
 var fileStorageClientLocal = new fileStorage("local");
 
-var fileName = "bhavesh.txt";
+var fileName = "sample.txt";
+var new_fileName = "sample2.txt"
 var data = "hello";
 
 fileStorageClientLocal.save(fileName, data, function (err) {
@@ -11,5 +12,32 @@ fileStorageClientLocal.save(fileName, data, function (err) {
 	}
 	else {
 		console.log("file saved successfully");
+	}
+})
+
+fileStorageClientLocal.open(fileName, function (err, data) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log(data);
+	}
+})
+
+fileStorageClientLocal.rename(fileName, new_fileName, function (err) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log("file renamed successfully");
+	}
+})
+
+fileStorageClientLocal.delete(fileName, function (err) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log("file deleted successfully");
 	}
 })
--- test/test-s3.js
+++ test/test-s3.js
@@ -3,13 +3,14 @@ var fileStorage = require('./file-storage');
 var options = {
 	accessKeyId: "accessKeyId",
 	secretAccessKey: "secretAccessKey",
-	bucket: "bucketName"
+	bucket: "bucket"
 }
 
 var fileStorageClientS3 = new fileStorage("s3", options);
 
 var fileName = "bhavesh.txt";
-var data = "Hi Bhavesh Here";
+var new_fileName = "sample.txt";
+var data = "Hello";
 
 
 fileStorageClientS3.save(fileName, data, function (err) {
@@ -18,5 +19,32 @@ fileStorageClientS3.save(fileName, data, function (err) {
 	}
 	else {
 		console.log("file saved successfully");
+	}
+})
+
+fileStorageClientS3.open(fileName, function (err, data) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log(data);
+	}
+})
+
+fileStorageClientS3.rename(fileName, new_fileName, function (err) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log("file renamed successfully");
+	}
+})
+
+fileStorageClientS3.delete(fileName, function (err) {
+	if(err) {
+		throw err;
+	}
+	else {
+		console.log("file deleted successfully");
 	}
 })
